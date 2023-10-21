6fe6595ff2d87a46695c0db8877393a3dac7f9e4
“ehijieifada” <sentbliss23@gmail.com>
2023-10-21 10:10:57 +0100

    first commit

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -8,24 +8,28 @@ const FooterBanner = ({ footerBanner: {discount, largeText1, largeText2, saleTim
     <div className="footer-banner-container">
       <div className="banner-desc">
         <div className="left">
+        <div class="footer-banner-image"><img 
+          src={urlFor(image)}
+          className="footer-banner-image"
+        /></div>
+
+
           <p>{discount}</p>
           <h3>{largeText1}</h3>
           <h3>{largeText2}</h3>
           <p>{saleTime}</p>
         </div>
         <div className="right">
+        <Link href={'/product/headphones'}>
+            <button type="button" class="shop-now">Shop Now</button>
+          </Link>
           <p>{smallText}</p>
           <h3>{midText}</h3>
           <p>{desc}</p>
-          <Link href={'/product/headphones'}>
-            <button type="button">{buttonText}
-            </button>
-          </Link>
+          
         </div>
-        <img 
-          src={urlFor(image)}
-          className="footer-banner-image"
-        />
+        
+
       </div>
     </div>
   )
--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -6,12 +6,14 @@ import { urlFor } from '../lib/client';
 const HeroBanner = ({ heroBanner, buttonText }) => {
   return (
     <div className="hero-banner-container">
+      <div class="hero-banner-image"><img src={urlFor(heroBanner.image)} alt="headphones"
+         className="hero-banner-image" /></div>
       <div>
+      
         <p className="beats-solo">{heroBanner.smallText}</p>
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
-        <img src={urlFor(heroBanner.image)} alt="headphones"
-         className="hero-banner-image" />
+        
 
         <div>
         <Link href={'/product/headphones'}>
